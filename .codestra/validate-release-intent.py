bb6a3f82476fd2f7ff864f022f193e9d894da68f
ingtrader21-spec <ingtrader21@gmail.com>
2026-09-24 19:21:17 -0400

    chore(trust): rederive PAS-64 source closure

--- .codestra/validate-release-intent.py
+++ .codestra/validate-release-intent.py
@@ -381,8 +381,8 @@ EXPECTED_REQUIRED_CHECK_SOURCE_CLOSURE_SHA256 = {
         "fcdd0d5c3832479b10deda8c6a6afca6"
     ),
     "ingtrader21-spec/Middleware-": (
-        "59c70756ffba0ab5195e36e52e41f74e76"
-        "eab21ca0ca5b376af6536d124a9db6"
+        "98c8937f6f497ae3a5b128124dbd894eb3"
+        "136c709b286b04f34e8302cff68f82"
     ),
     "appolon1908-hue/codestra": (
         "4e3ea69c3ec2a4bd6e4b50395672f44d"
